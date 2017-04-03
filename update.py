64a7b80a139fb3a27758a8a7b14efef0de139c03
SakaDream <bahaimt@gmail.com>
2017-04-03 09:59:16 +0000

    Fix error

--- update.py
+++ update.py
@@ -1,3 +1,3 @@
-#!usr/bin/python
+#!/usr/bin/python
 from os import system
-system("sudo apt-get update && sudo apt-get upgrade -y  && sudo apt-get autoremove && sudo apt-get autoclean")
+system("sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove && sudo apt-get autoclean")
