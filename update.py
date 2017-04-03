7b5f809aef178cd65c1e2adef19497a68967fc75
root <root@ubuntu-512mb-sgp1-01.localdomain>
2017-04-03 08:36:11 +0000

    Add -y option

--- update.py
+++ update.py
@@ -1,3 +1,3 @@
 #!usr/bin/python
 from os import system
-system("sudo apt-get update && sudo apt-get upgrade && sudo apt-get autoremove && sudo apt-get autoclean")
+system("sudo apt-get update && sudo apt-get upgrade -y  && sudo apt-get autoremove && sudo apt-get autoclean")
